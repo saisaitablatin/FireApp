75b2e433e8907baf02634c060ac6e0888175590c
saisaitablatin <202080120@psu.palawan.edu.ph>
2024-06-11 11:15:18 -0400

    successful chart setup

--- FireApp/views.py
+++ FireApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+class ChartView(ListView):
+    template_name = "chart.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        pass
