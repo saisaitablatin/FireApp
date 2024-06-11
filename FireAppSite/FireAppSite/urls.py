0a35017be14a7ec352b36ae923f5d3d141df2968
saisaitablatin <202080120@psu.palawan.edu.ph>
2024-06-11 11:52:20 -0400

    dynamic pie chart

--- FireAppSite/FireAppSite/urls.py
+++ FireAppSite/FireAppSite/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from FireApp.views import ChartView
+from FireApp.views import ChartView, PieCountbySeverity
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("dashboard_chart/", ChartView.as_view(), name="dashboard-chart"),
+    path("chart/", PieCountbySeverity, name="chart"),
 ]
